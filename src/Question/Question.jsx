9d48fcdb43f9cfeb3409f09093e0e9d96021c4e9
deepak200-co <75674047+deepak200-co@users.noreply.github.com>
2024-03-18 05:21:56 +0530

    Update Question.jsx

--- src/Question/Question.jsx
+++ src/Question/Question.jsx
@@ -69,7 +69,7 @@ function QuestionComponent() {
                 <div className="happy-gif-text">
                     <img src="https://media.tenor.com/E4od4SG3hjMAAAAi/bunny-bunnies.gif" alt='Kiss GiF' />
                     <p className="fade-in"><h2>Awww! Baby love you😘😘🥰💗</h2></p>
-                    //<img src="https://media.tenor.com/eiMqmu0vXz8AAAAi/kiss.gif" alt="Happy GIF" />
+                  /*  <img src="https://media.tenor.com/eiMqmu0vXz8AAAAi/kiss.gif" alt="Happy GIF" />*/
                     </div>
             )}
             {answer === '' && !loading && (
